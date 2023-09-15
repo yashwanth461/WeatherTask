31ad2eeaab4ed9010ed427f919f37a6bf2e3dc21
yashwanth461 <yashwanthkatta31@gmail.com>
2023-09-15 11:40:34 +0530

    files added

---
+++ task/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from './Components/Weather';
+
+
+function App() {
+  return (
+    <div>
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
